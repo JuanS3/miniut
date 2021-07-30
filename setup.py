a5d160faa0d873503f61590b17fb53accf17f5af
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2021-07-29 22:24:32 -0500

    docs: :construction: add readme template

--- setup.py
+++ setup.py
@@ -1,10 +1,20 @@
 from setuptools import find_packages, setup
 
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
-    name='utils',
+    name='utilities',
     packages=find_packages(),
-    version='0.1.0',
-    description='Paquete de utilidades para el trabajo de los proyectos de Rollup Consulting',
-    author='Rollup Consulting',
+    version='0.0.1',
+    description='Paquete de utilidades',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Juan Sebastián Martínez Serna',
 )
 
