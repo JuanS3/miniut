f14191a9d01aafdb5be84e68e5a743200452e41a
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2021-07-30 20:06:36 -0500

    docs: :memo:

--- setup.py
+++ setup.py
@@ -10,11 +10,22 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='utilities',
-    packages=find_packages(),
     version='0.0.1',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
     description='Paquete de utilidades',
     long_description=long_description,
     long_description_content_type='text/markdown',
     author='Juan Sebastián Martínez Serna',
+    url='https://github.com/JuanS3/utilities',
+    project_urls={
+        'Bug Tracker' : 'https://github.com/JuanS3/utilities/issues',
+        'Wiki' : 'https://github.com/JuanS3/utilities/wiki'
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8"
 )
-
