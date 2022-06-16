0e38424bc94bd984913a9afbed6fed0a57e65684
Sebastián Martínez <sebastian.martinez.serna@gmail.com>
2022-06-15 21:29:10 -0500

    style: :memo: change __version__ definition

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 from setuptools import find_packages, setup
+from os import path
+import re
 
 
 # read the contents of your README file
-from os import path
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+def get_version(prop):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop), open( 'src/miniut/__init__.py').read())
+    return result
 
 setup(
     name='miniut',
-    version='0.1.1',
+    version = get_version('__version__'),
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     description='Paquete de utilidades',
